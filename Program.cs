d8138762dde7ebc587aac69ff773457345507a37
Jan Paolo Go <go.janpaolo@gmail.com>
2026-02-28 07:35:07 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@ logger.LogWarning("jantest log Application Starting Up");
 app.MapGet("/", (string x) =>
 {
     logger.LogWarning($"Hello world jantest log {x}");
-    return $"Hello World: {x}";
+    return $"Hello World2: {x}";
 });
 
 app.Run();
+
 
